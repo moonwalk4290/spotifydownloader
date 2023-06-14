30cbabd69e0b8810ebbb4760ab2403e10adc7d38
Yusuf Seçkin <136327413+moonwalk4290@users.noreply.github.com>
2023-06-14 07:11:15 +0300

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -1,1 +1,15 @@
+from pytube import YouTube , Search
+import os
 
+def songSearch(name,artist):
+    search = Search(f"{name} - {artist}")
+    return search.results[0].watch_url
+    
+
+
+def songDownload(name,artist,playlist_name):
+    url = songSearch(name,artist)
+    yt = YouTube(url)
+    video = yt.streams.filter(only_audio=True).first()
+    path = os.path.join(".",playlist_name)
+    video.download(output_path=path)
