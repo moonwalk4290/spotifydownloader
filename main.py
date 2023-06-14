5736811d2b9e1110ef473a7892d5e817a3cbb13a
Yusuf Seçkin <136327413+moonwalk4290@users.noreply.github.com>
2023-06-14 07:10:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import spotify as sp
+import youtube as yt
 
+playlistUrl = input("Enter your playlist url: ")
+(uri,playlistName,songAmount) = sp.getPlaylistInformation(playlistUrl)
+
+for i in range(songAmount):
+    name, artist= sp.getSongName(uri,i)
+    name = yt.songDownload(name, artist, playlistName)
+    
+print(f"Downloaded {songAmount} songs from {playlistName} playlist")
