bde53707499ee4269b2e2ea32ef915c03d223b44
Yusuf Seçkin <136327413+moonwalk4290@users.noreply.github.com>
2023-06-14 17:23:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
 import spotify as sp
 import youtube as yt
 
-playlistUrl = input("Enter your playlist url: ")
-(uri,playlistName,songAmount) = sp.getPlaylistInformation(playlistUrl)
+def songDownload():
+    songUrl = input("Enter the song url: ")
+    name , artist = sp.getSongName(songUrl)
+    yt.songDownload(name, artist, name)
+    print(f"Downloaded {name} - {artist} .")
 
-for i in range(songAmount):
-    name, artist= sp.getSongName(uri,i)
-    name = yt.songDownload(name, artist, playlistName)
-    
-print(f"Downloaded {songAmount} songs from {playlistName} playlist")
+
+def playlistDownload():
+    playlistUrl = input("Enter your playlist url: ")
+    (uri,playlistName,songAmount) = sp.getPlaylistInformation(playlistUrl)
+
+    for i in range(songAmount):
+        name, artist= sp.getSongNameonPlaylist(uri,i)
+        yt.songDownload(name, artist, playlistName)
+        print(f"Downloaded {name} - {artist} .")
+        
+    print(f"Downloaded {songAmount} songs from {playlistName} playlist")
+
+
+ans = input('''
+1- Song Download
+2- Playlist Download
+
+Press different key for exit.
+''')
+
+
+if (ans == "1"):
+    songDownload()
+elif (ans == "2"):
+    playlistDownload()
+else:
+    pass
