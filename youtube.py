9e811824148ffa2961202c281a0cff8438098fb7
Yusuf Seçkin <136327413+moonwalk4290@users.noreply.github.com>
2023-06-14 10:09:11 +0300

    Update youtube.py
    Fixed .mp3 problem.

--- youtube.py
+++ youtube.py
@@ -1,15 +1,24 @@
-from pytube import YouTube , Search
+from pytube import Search, YouTube
 import os
+import subprocess
 
 def songSearch(name,artist):
     search = Search(f"{name} - {artist}")
     return search.results[0].watch_url
     
 
-
 def songDownload(name,artist,playlist_name):
-    url = songSearch(name,artist)
-    yt = YouTube(url)
-    video = yt.streams.filter(only_audio=True).first()
     path = os.path.join(".",playlist_name)
-    video.download(output_path=path)
+    url = songSearch(name,artist) 
+    yt = YouTube(url)
+    audio = yt.streams.filter(only_audio=True).first()
+    down = audio.download(path)
+    newPath= os.path.join(path,f"{name}_{artist}.mp4")
+    os.rename(down,newPath)
+    subprocess.run([
+        "ffmpeg",
+        "-i",
+        newPath,
+        os.path.join(path,f"{name}.mp3")
+    ])
+    os.remove(newPath)
